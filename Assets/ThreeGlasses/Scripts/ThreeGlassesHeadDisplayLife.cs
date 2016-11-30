74c75bac8c5cf7390bc39ac73363d33ea3ace896
TianShengLi <tiansheng.li@3glasses.com>
2016-11-30 18:12:36 +0800

    fix plugin destroy
    Signed-off-by: TianShengLi <tiansheng.li@3glasses.com>

--- Assets/ThreeGlasses/Scripts/ThreeGlassesHeadDisplayLife.cs
+++ Assets/ThreeGlasses/Scripts/ThreeGlassesHeadDisplayLife.cs
@@ -3,18 +3,18 @@ using System.Collections;
 
 namespace ThreeGlasses
 { 
-    public class ThreeGlassesHeadDisplayLife : MonoBehaviour {
-
-	    // Use this for initialization
-	    void Awake () {
+    public class ThreeGlassesHeadDisplayLife : MonoBehaviour
+    {
+	    void Awake ()
+        {
             ThreeGlassesUtils.Log("ThreeGlassesHeadDisplayLife init");
             ThreeGlassesDllInterface.SZVRPluginInit();
         }
-	
-	    // Update is called once per frame
-	    void OnDestroy() {
-            ThreeGlassesUtils.Log("ThreeGlassesHeadDisplayLife destroy");
-            ThreeGlassesDllInterface.SZVRPluginDestroy();        
+
+        void OnApplicationQuit()
+        {
+            ThreeGlassesUtils.Log("ThreeGlassesHeadDisplayLife application quit");
+            ThreeGlassesDllInterface.SZVRPluginDestroy();
         }
     }
 }
