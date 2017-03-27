ef4804758d47651541e435532e64cc9bf5ab8a3e
TianShengLi <tiansheng.li@3glasses.com>
2017-03-27 10:24:15 +0800

    clean code
    Signed-off-by: TianShengLi <tiansheng.li@3glasses.com>

--- Assets/ThreeGlasses/Scripts/ThreeGlassesWand.cs
+++ Assets/ThreeGlasses/Scripts/ThreeGlassesWand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//#define DEBUG_WAND
+using System;
 using UnityEngine;
 using System.Collections;
 namespace ThreeGlasses
@@ -82,57 +83,75 @@ namespace ThreeGlasses
             pack.type = type;
         }
 
-        // // update wand info
-        // public void Update()
-        // {
-        //     // update position
-        //     float x = 0, y = 0, z = 0, w = 1;
-        //     if (0 == ThreeGlassesDllInterface.szvrGetWandsPositonWithVector((int)pack.type, ref x, ref y, ref z))
-        //     {
-        //         Vector3 vec = new Vector3(x, y, z)/-1000f;
-        //         if (ThreeGlassesUtils.CheckNaN(vec))
-        //         {
-        //             pack.position = vec;
-        //             // Debug.Log("type=" + pack.type + "    vec=" + vec);
-        //         }
-        //     }
-        //     // update rotation
-        //     if (0 == ThreeGlassesDllInterface.SZVR_GetWandRotate((int)pack.type, ref x, ref y, ref z, ref w))
-        //     {
-        //         Quaternion quat = new Quaternion(x, -y, z, -w);
-        //         if (ThreeGlassesUtils.CheckNaN(quat))
-        //         {
-        //             pack.rotation = quat;
-        //         }
-        //     }
+#if DEBUG_WAND
+        public void Update()
+        {
+            // update position
+            float[] rotate_result = new float[8] {0, 0, 0, 1, 0, 0, 0, 1};
+            float[] pos_result = new float[6] {0, 0, 0, 0, 0, 0};
 
-        //     // update key
-        //     int trigger_value = 0;
-        //     if (0 == ThreeGlassesDllInterface.szvrGetWandsTriggerValue((int)pack.type, ref trigger_value))
-        //     {
-        //         pack.triggerProcess = 1.0f - (trigger_value / (float)255.0);
-        //     }
-            
-        //     int stick_x = 128, stick_y = 128;
-        //     if (0 == ThreeGlassesDllInterface.szvrGetWandsStickValue((int)pack.type, ref stick_x, ref stick_y))
-        //     {
-        //         //                 int left = (int)Mathf.Clamp(((stickTemp[1] - 127) * 1.2f), -128, 128);
-        //         //                 int right = (int)Mathf.Clamp(((stickTemp[2] - 127) * 1.2f), -128, 128);
-        //         //                 left /= 16;//-8~8
-        //         //                 right /= 16;
-        //         // 
-        //         //                 pack.stick[0] = left / 8.0f;
-        //         //                 pack.stick[1] = -right / 8.0f;
-        //         pack.stick[0] = ((stick_x / (float)255.0) - 0.5f)*2.0f;
-        //         pack.stick[1] = (-(stick_y / (float)255.0) + 0.5f)*2.0f;
-        //     }
+            if (0 == ThreeGlassesDllInterface.SZVR_GetWandPos(pos_result))
+            {
+                Vector3 left_pos = new Vector3(pos_result[0], pos_result[1],
+                                       pos_result[2]) / -1000f;
+                Vector3 right_pos = new Vector3(pos_result[3], pos_result[4],
+                                        pos_result[5]) / -1000f;
 
-        //     int keyStatus = 0;
-        //     if (0 == ThreeGlassesDllInterface.szvrGetWandsButtonState((int)pack.type, ref keyStatus))
-        //     {
-        //         pack.keyStatus = keyStatus;
-        //     }
-        // }
+                switch (pack.type)
+                {
+                    case InputType.LeftWand:
+                        if (ThreeGlassesUtils.CheckNaN(left_pos))
+                        {
+                            pack.position = left_pos;
+                            Debug.Log("type=" + pack.type + "    vec="
+                                      + left_pos);
+                        }
+                        break;
+                    case InputType.RightWand:
+                        if (ThreeGlassesUtils.CheckNaN(right_pos))
+                        {
+                            pack.position = right_pos;
+                            Debug.Log("type=" + pack.type + "    vec="
+                                      + right_pos);
+                        }
+                        break;
+                    case InputType.HMD:
+                        break;
+                }
+            }
+
+            // update rotation
+            if (0 == ThreeGlassesDllInterface.SZVR_GetWandRotate(rotate_result))
+            {
+                Quaternion left_quat = new Quaternion(rotate_result[0],
+                    rotate_result[1], rotate_result[2], rotate_result[3]);
+                Quaternion right_quat = new Quaternion(rotate_result[0],
+                    rotate_result[1], rotate_result[2], rotate_result[3]);
+
+                switch (pack.type)
+                {
+                    case InputType.LeftWand:
+                        if (ThreeGlassesUtils.CheckNaN(left_quat))
+                        {
+                            pack.rotation = left_quat;
+                            Debug.Log("type=" + pack.type + "    vec="
+                                      + left_quat);
+                        }
+                        break;
+                    case InputType.RightWand:
+                        if (ThreeGlassesUtils.CheckNaN(right_quat))
+                        {
+                            pack.rotation = right_quat;
+                            Debug.Log("type=" + pack.type + "    vec="
+                                      + right_quat);
+                        }
+                        break;
+                    case InputType.HMD:
+                        break;
+                }
+            }
+        }
+#endif
 
         public void UpdatePos(float[] pos)
         {
@@ -190,17 +209,6 @@ namespace ThreeGlasses
         public void SetMotor(ushort level)
         {
             pack.SetMotor(level);
-        }
-
-        private static bool checkFloat(float v)
-        {
-            var status = !float.IsNaN(v);
-
-            if (v > 500.0f || v < -500.0f)
-            {
-                status = false;
-            }
-            return status;
         }
     }
 }
